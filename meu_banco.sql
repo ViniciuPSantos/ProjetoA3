b1240c6728712da02a42686f4d575ac0aa7c9807
Vinicius <95146535+ViniciuPSantos@users.noreply.github.com>
2025-05-12 11:50:27 -0300

    Create meu_banco.sql

---
+++ meu_banco.sql
@@ -0,0 +1,8 @@
+create DATABASE meu_banco;
+use meu_banco;
+CREATE TABLE Usuario (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     nome VARCHAR(100) not null,
+     email VARCHAR(100) not null unique,
+     senha VARCHAE(255) not null
+);
