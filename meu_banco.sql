4e66f5a5c8ead3baa3ba50b9bac761d75d90f983
Vinicius <95146535+ViniciuPSantos@users.noreply.github.com>
2025-05-12 11:57:30 -0300

    Update meu_banco.sql
    correção de erros

--- meu_banco.sql
+++ meu_banco.sql
@@ -3,6 +3,6 @@ use meu_banco;
 CREATE TABLE Usuario (
      id INT AUTO_INCREMENT PRIMARY KEY,
      nome VARCHAR(100) not null,
-     email VARCHAR(100) not null unique,
-     senha VARCHAE(255) not null
+     email VARCHAR(100) not null UNIQUE,
+     senha VARCHAR(255) not null
 );
