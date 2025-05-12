7abf323bdc5aa7fcb58ac7eba9ea094fd5c0d329
Vinicius <95146535+ViniciuPSantos@users.noreply.github.com>
2025-05-12 11:22:56 -0300

    Update UsuarioDAO.java
    add o autenticar

--- UsuarioDAO.java
+++ UsuarioDAO.java
@@ -1,14 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
 package com.mycompany.proejtoa3;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
+import java.sql.ResultSet;
 
 public class UsuarioDAO {
     private static final String URL = "jdbc:mysql://localhost:3306/meu_banco";
     private static final String USUARIO = "root";
-    private static final String SENHA = "senha";
+    private static final String SENHA = "anima123";
+    
+    public boolean autenticar(String email, String senha){
+        boolean autenticado = false;
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/meu_banco", "root", "senha");
+            
+            String sql = "SELECT * FROM usuario WHERE email = ? AND senha = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+            
+            ResultSet rs = stmt.executeQuery();
+            
+            if(rs.next()){
+                autenticado = true;
+            }
+            
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            
+            
+        }
+        
+        return autenticado;
+    }
+    
     
     public void cadastrarUsuario(String nome,String email,String senha){
         String sql = "INSERT INTO usuario(nome, email, senha) VALUES (?,?,?)";
