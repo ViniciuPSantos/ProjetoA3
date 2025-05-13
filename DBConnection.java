492a08e488a6495158b8a1b36f340bf0bfbfacb4
Vinicius <95146535+ViniciuPSantos@users.noreply.github.com>
2025-05-13 11:58:21 -0300

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/meu_banco";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "anima123";
+    
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException("Driver JDBC não encontrado", ex);
+        }
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
+}
